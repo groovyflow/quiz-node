b0113f24fcf2ec22806cbf629ceeee2a50bb37de
groovyflow <groovyflow@gmail.com>
2014-05-15 16:58:24 -0400

    Added route to be used when the last question has been asked

--- public/js/quizapp.js
+++ public/js/quizapp.js
@@ -2,12 +2,16 @@ var quizapp = angular.module("quizapp", [])
 
 quizapp.config(["$routeProvider",
     function ($routeProvider) {
-        $routeProvider.when('/questions', {
+        $routeProvider.when('/results', {
+            controller: "ResultsCtrl",
+            templateUrl: "templates/results.html"
+        }).when('/questions', {
             controller: "QuestionsCtrl",
             templateUrl: "templates/questions.html"
         }).otherwise({redirectTo: "/questions"});
     }]);
 
+//Should we use promises here? https://docs.angularjs.org/api/ng/service/$q
 quizapp.service("NextQuestion", function($http){
   this.initial = function(prevQuestion, answer, callback) {
       $http({
@@ -24,21 +28,39 @@ quizapp.service("NextQuestion", function($http){
 
 });
 
-quizapp.controller("QuestionsCtrl", function ($scope, NextQuestion) {
-    function dataBind(data){
-        $scope.data = data.data;
-    }
+//Stackoverflow on sharing data between controllers: http://stackoverflow.com/questions/18227090/angularjs-sharing-data-between-controllers
+//Besides the DataSharing service below it suggests:
+// $rootScope.$broadcast('UPDATE_CLIENT_DATA', clientDataObject);
+// and listening to the broadcast with: $scope.$on('UPDATE_CLIENT_DATA', function ( event, clientDataObject ) { ... });
+quizapp.factory("DataSharing", function() {
+    return {}
+});
+
+quizapp.controller("ResultsCtrl", function($scope, DataSharing){
+   $scope.data = DataSharing.data;
+});
+
+quizapp.controller("QuestionsCtrl", function ($scope, $location, NextQuestion, DataSharing) {
     $scope.data = {};
+    function bindAndReact(data){
+        console.log("status is " + data.status);
+        if(data.status === 'continue'){
+            $scope.data = data.data;
+        }else{
+          DataSharing.data = data.result;
+          $location.path("/results");
+        }
+    }
 
     //Passing in $scope.data and binding there doesn't work!!
-    NextQuestion.initial("NONE", "", dataBind);
+    NextQuestion.initial("NONE", "", bindAndReact);
 
     $scope.nextQuestion = function (answer) {
         if (isEmpty(answer)) {
             $('#questionsForm .error').text("Please choose an answer before submitting").show().fadeOut(3000);
         }
         else {
-            NextQuestion.initial($scope.data, answer, dataBind);
+            NextQuestion.initial($scope.data, answer, bindAndReact);
         }
     };
 });
