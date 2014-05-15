a8392869a0ce672f9021ef330e57642c2527ea58
groovyflow <groovyflow@gmail.com>
2014-05-15 14:17:50 -0400

    Now using a Service

--- public/js/quizapp.js
+++ public/js/quizapp.js
@@ -8,57 +8,37 @@ quizapp.config(["$routeProvider",
         }).otherwise({redirectTo: "/questions"});
     }]);
 
-//To do.  Use a service or pull out the frequent $http call into an inner function
-//No more data.data
-quizapp.controller("QuestionsCtrl", function ($scope, $http) {
-     //We want to use this function, but need to do something with the promise.  See app.js, and see http://stackoverflow.com/questions/17414093/jquerys-when-done-for-angularjs
-    //$q, mentioned there, is useful when waiting for many things to return, even if we don't need it here
-    //Or make this a service.
-    //Or just pass in a function that does something in the success, and one that does something in the error.
-    /*function remote(prevQuestion, answer) {
-        $http({
-            method: "POST",
-            url: 'quiz/reply',
-            responseType: 'json',
-            data: {prevQuestion: prevQuestion || 'NONE', answer: answer || ""}
-        }).success(function (data, status) {
-                $scope.data = data.data;
-            }).error(function (data, status) {
-                $('#questionsForm .error').text("Unsuccessful request").show().fadeOut(3000);
-            });
-    } */
+quizapp.service("NextQuestion", function($http){
+  this.initial = function(prevQuestion, answer, callback) {
+      $http({
+      method: "POST",
+      url: 'quiz/reply',
+      responseType: 'json',
+      data: {prevQuestion: prevQuestion || 'NONE', answer: answer || ""}
+  }).success(function (data, status) {
+          callback(data);
+      }).error(function (data, status) {
+          $('#questionsForm .error').text("Unsuccessful request").show().fadeOut(3000);
+      });
+    }
 
+});
+
+quizapp.controller("QuestionsCtrl", function ($scope, NextQuestion) {
+    function dataBind(data){
+        $scope.data = data.data;
+    }
     $scope.data = {};
-    $http({
-        method: "POST",
-        url: 'quiz/reply',
-        responseType: 'json',
-        data: {prevQuestion: 'NONE', answer: ""}
-    }).success(function (data, status) {
-            $scope.data = data.data;
-        }).error(function (data, status) {
-            $('#questionsForm .error').text("Unsuccessful request").show().fadeOut(3000);
-        });
+
+    //Passing in $scope.data and binding there doesn't work!!
+    NextQuestion.initial("NONE", "", dataBind);
 
     $scope.nextQuestion = function (answer) {
         if (isEmpty(answer)) {
             $('#questionsForm .error').text("Please choose an answer before submitting").show().fadeOut(3000);
         }
         else {
-            console.log("The chosen answer is " + answer);
-            //console.long("The question is " + $scope.data.data.question)
-            $http({
-                method: "POST",
-                url: 'quiz/reply',
-                responseType: 'json',
-                data: {prevQuestion: $scope.data, answer: answer}
-            }).success(function (data, status) {
-                    $scope.data = data.data;
-                }).error(function (data, status) {
-                    $('#questionsForm .error').text("Unsuccessful request").show().fadeOut(3000);
-                });
+            NextQuestion.initial($scope.data, answer, dataBind);
         }
     };
-
-
 });
